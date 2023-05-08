056c8efc285ae531bbfd2d6427e15763edd71870
yohrannes <yohrannes@gmail.com>
2023-05-08 13:36:36 -0300

    Atualização de dados
    finalizando o script de criação de tabelas, criando um novo script para conter somente os scripts de criação de chaves estrangeiras.

--- criacao-de-tabelas.sql
+++ criacao-de-tabelas.sql
@@ -28,13 +28,13 @@ valor_pago float
 create table tb_venda(
 id_cliente integer,
 id_venda int not null auto_increment primary key,
-constraint id_cliente foreign key (id_cliente) references tb_cliente (id_cliente), -- linkando id_cliente com a tb_vendas
+-- constraint id_cliente foreign key (id_cliente) references tb_cliente (id_cliente), -- linkando id_cliente com a tb_vendas
 dt_venda date,
 valor_venda float,
 id_pizza int,
-constraint id_pizza foreign key (id_pizza) references tb_cardapio (id_pizza), -- linkando id_pizza com tb_cardapio
-id_pagamento int,
-constraint id_pagamento foreign key (id_pagamento) references tb_pagamento (id_pagamento)-- linkando id_pagamento com tb_cardapio
+-- constraint id_pizza foreign key (id_pizza) references tb_cardapio (id_pizza), -- linkando id_pizza com tb_cardapio
+id_pagamento int
+-- constraint id_pagamento foreign key (id_pagamento) references tb_pagamento (id_pagamento)-- linkando id_pagamento com tb_cardapio
 )engine= Innodb default charset = utf8mb4;
 
 create table tb_ingredientes(
@@ -47,7 +47,7 @@ id_pizza integer not null auto_increment primary key,
 nome_pizzza varchar(10),
 tipo_pizzza varchar(10),
 id_ingrediente int,
-constraint id_ingrediente foreign key (id_ingrediente) references tb_ingredientes (id_ingrediente), -- Linkando id_ingrediente com tb_ingredientes
+-- constraint id_ingrediente foreign key (id_ingrediente) references tb_ingredientes (id_ingrediente), -- Linkando id_ingrediente com tb_ingredientes
 valor_pizza float
 )engine= Innodb default charset = utf8mb4;
 
@@ -55,17 +55,8 @@ create table tb_entrega(
 id_entrega int not null auto_increment primary key,
 id_funcionario integer,
 id_cliente integer,
-id_venda integer,
-constraint id_funcionario foreign key (id_funcionario) references tb_funcionario (id_funcionario), -- Linkando id_funcionario com tb_funcionario
-constraint id_cliente_segfk foreign key (id_cliente) references tb_cliente (id_cliente), /* Linkando id_cliente com tb_cliente obs: a chave estranjeira id_cliente
-já existe na tabela tb_venda, pos isso é preciso criar um nome diferente para esta chave, chamei fe id_cliente_segfk */
-constraint id_venda foreign key (id_venda) references tb_venda (id_venda) -- Linkando id_venda com tb_venda
+id_venda integer
+-- constraint id_funcionario foreign key (id_funcionario) references tb_funcionario (id_funcionario), -- Linkando id_funcionario com tb_funcionario
+/* constraint id_cliente_segfk foreign key (id_cliente) references tb_cliente (id_cliente), Linkando id_cliente com tb_cliente obs: a chave estranjeira id_cliente já existe na tabela tb_venda, pos isso é preciso criar um nome diferente para esta chave, chamei fe id_cliente_segfk */
+-- constraint id_venda foreign key (id_venda) references tb_venda (id_venda) -- Linkando id_venda com tb_venda
 )engine= Innodb default charset = utf8mb4;
-
-
-
-use pizzaria;
-drop table tb_entrega;
-show tables;
-describe tb_cliente;
-
---
+++ relacionando-com-chaves-extranjeras.sql
@@ -0,0 +1,8 @@
+-- Linkando id_cliente com a tb_vendas
+-- Linkando id_pizza com tb_cardapio
+-- Linkando id_pagamento com tb_cardapio
+-- Linkando id_ingrediente com tb_ingredientes
+-- Linkando id_funcionario com tb_funcionario
+/* Linkando id_cliente com tb_cliente obs: a chave estranjeira id_cliente já existe na tabela tb_venda, 
+por isso é preciso criar um nome diferente para esta chave chamei fe id_cliente_segfk */
+-- Linkando id_venda com tb_venda
