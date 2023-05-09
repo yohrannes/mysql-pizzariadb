04089fa7f45b6b025c817c882cfff6e0ff20d9ce
yohrannes <yohrannes@gmail.com>
2023-05-09 08:53:23 -0300

    Update relacionando-com-chaves-estrangeiras.sql

--- relacionando-com-chaves-estrangeiras.sql
+++ relacionando-com-chaves-estrangeiras.sql
@@ -1,8 +1,14 @@
--- Linkando id_cliente com a tb_vendas
--- Linkando id_pizza com tb_cardapio
--- Linkando id_pagamento com tb_cardapio
--- Linkando id_ingrediente com tb_ingredientes
--- Linkando id_funcionario com tb_funcionario
-/* Linkando id_cliente com tb_cliente obs: a chave estranjeira id_cliente já existe na tabela tb_venda, 
-por isso é preciso criar um nome diferente para esta chave chamei fe id_cliente_segfk */
--- Linkando id_venda com tb_venda
+-- Estabelecimento de uma relação entre tabelas id_cliente com a tb_vendas
+
+-- Estabelecimento de uma relação entre tabelas id_pizza com tb_cardapio
+
+-- Estabelecimento de uma relação entre tabelas id_pagamento com tb_cardapio
+
+-- Estabelecimento de uma relação entre tabelas id_ingrediente com tb_ingredientes
+
+-- Estabelecimento de uma relação entre tabelas id_funcionario com tb_funcionario
+
+/* Estabelecimento de uma relação entre tabelas id_cliente com tb_cliente obs: a chave estranjeira id_cliente já existe na tabela tb_venda, 
+por isso é preciso criar um nome diferente para esta chave chamei de id_cliente_segfk */
+
+-- Estabelecimento de uma relação entre tabelas id_venda com tb_venda
